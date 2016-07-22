569eadbcece184aa8e6c0cb07cf00202d8d30996
Danny Libin <omegasol11@gmail.com>
2016-07-21 19:29:46 -0500

    add: skill bars in about section

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -11,11 +11,71 @@ import { TransitionService } from '../shared/transition.service';
   directives: [ROUTER_DIRECTIVES]
 })
 export class AboutMeComponent implements OnInit {
+  languages = [];
+  frameworks = [];
 
   constructor(private transitionService: TransitionService) { }
 
   ngOnInit() {
     this.transitionService.transition();
+    this.showSkills();
+    this.animateSkillbar();
+  }
+
+  animateSkillbar() {
+    
+  }
+
+  showSkills() {
+    this.languages = [
+      {
+        name: 'HTML',
+        amount: 95
+      },
+      {
+        name: 'SASS/CSS',
+        amount: 90
+      },
+      {
+        name: 'Javascript',
+        amount: 90
+      },
+      {
+        name: 'Typescript',
+        amount: 85
+      },
+      {
+        name: 'Java',
+        amount: 80
+      },
+      {
+        name: 'Python',
+        amount: 60
+      },
+      {
+        name: 'Ruby',
+        amount: 30
+      }
+    ];
+
+    this.frameworks = [
+      {
+        name: 'Angular 2.x',
+        amount: 90
+      },
+      {
+        name: 'React',
+        amount: 70
+      },
+      {
+        name: 'jQuery',
+        amount: 65
+      },
+      {
+        name: 'Django',
+        amount: 20
+      }
+    ]
   }
 
 }
