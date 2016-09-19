cc7151f9382da52712c1c135c68d26626c4c1eb0
Danny Libin <omegasol11@gmail.com>
2016-09-19 09:21:23 -0500

    forms coming soon

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -5,7 +5,11 @@ import { TransitionService } from '../shared/transition.service';
 @Component({
   moduleId: module.id,
   selector: 'forms',
-  templateUrl: 'forms.component.html',
+  template: `
+    <div [class.page-transition]="transitionService.isTransitioning()">
+      <h3 class="text-center">Forms coming soon...</h3>
+    </div>
+  `,
   styleUrls: ['forms.component.css']
 })
 export class FormsComponent implements OnInit {
